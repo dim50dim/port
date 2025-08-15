83cc3e733529c592ae957fa6400b09a73e1a9cb2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 10:04:38 +0200

    was mistake related with path.

--- port/src/assets/components/Navbar.jsx
+++ port/src/assets/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Logo from "../logo1.png"
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-               
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+          <div>
+              <img src={Logo} alt="Logo" />
+          </div>         
     </div>
   )
 }
