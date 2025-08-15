55c52efd8ca80168821c2e222908252af8489272
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 14:43:17 +0200

    added some styles to h1 h2 and p tags.

--- port/src/assets/components/Home.jsx
+++ port/src/assets/components/Home.jsx
@@ -6,14 +6,17 @@ const Home = () => {
                {/* Container  */}
 
                <div className='ma-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p>Hi, my name</p>
-                <h1>Zhelezkov Dmytro</h1>
-                <h2>I'm a Developer</h2>
+                <p className='text-pink-600 '>Hi, my name</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Zhelezkov Dmytro</h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Developer</h2>
                     <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
-        </p>
+                      I’m a full-stack developer specializing in building (and occasionally
+                      designing) exceptional digital experiences. Currently, I’m focused on
+                      building responsive full-stack web applications.
+                       </p>
+                       <div>
+                           <button >View Work <HiArrowNarrowRight/></button>
+                       </div>
                </div>
     </div>
   )
