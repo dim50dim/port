b8b9e99a389f4782dbb3d8d5c8ecaed7d181aeec
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 18:20:05 +0200

    added some classes to icons-block

--- port/src/assets/components/Skill.jsx
+++ port/src/assets/components/Skill.jsx
@@ -3,15 +3,15 @@ import HTML from "@/assets/html.png"
 const Skill = () => {
   return (
     <div>
-   <div name="skills" className='bg-[#0a192f]'>
+   <div name="skills" className='bg-[#0a192f] text-gray-300'>
            {/* Container ? */}
            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-               <div className='text-white' >
+               <div >
                   <p className='text-4xl  py-4 font-bold inline border-b-4 border-pink-600'>Experience</p>
-                  <p> These are things I've used to build peculiar stuff</p>
+                  <p className='py-4'> These are things I've used to build peculiar stuff</p>
                </div>
-                 <div>
-                       <div>
+                 <div className='w-full grid grid-col-2 sm:grid-cols4 gap-4 text-center py-8'>
+                       <div className='shadow-md shadow-[#040c16] hover:scale-100 duration-500'>
                             <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                        </div>
                  </div>
