f1ea0122530adf3b4ebf506c696ac6f3afee67c0
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 18:00:41 +0200

    implemented icons from assets , used @ for that.

--- port/src/App.jsx
+++ port/src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./assets/components/About"
 import Home from "./assets/components/Home"
 import Navbar from "./assets/components/Navbar"
+import Skill from "./assets/components/Skill"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Navbar/>
         <Home/>
         <About/>
+        <Skill/>
     </>
   )
 }
--- port/src/assets/components/Skill.jsx
+++ port/src/assets/components/Skill.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import HTML from "@/assets/html.png"
 const Skill = () => {
   return (
     <div>
-          <div>Skills</div>
+   <div name="skills">
+           {/* Container ? */}
+           <div>
+               <div>
+                  <p>Experience</p>
+                  <p> These are things I've used to build peculiar stuff</p>
+               </div>
+                 <div>
+                       <div>
+                            <img className='w-[50px]' src={HTML} alt="HTML icon" />
+                       </div>
+                 </div>
+           </div>
+   </div>
     </div>
   )
 }
