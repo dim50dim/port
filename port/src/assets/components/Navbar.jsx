3c1941147249f17e81b59b9ac4447704f3e7c95a
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 14:32:22 +0200

    implemented four different links such as github linkedin resume email

--- port/src/assets/components/Navbar.jsx
+++ port/src/assets/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Logo from "@/assets/logo1.png"
 import {FaBars, FaTimes,FaGithub,FaLinkedin} from "react-icons/fa";
 import {BsFillPersonLinesFill} from "react-icons/bs"
+import { HiOutlineMail } from 'react-icons/hi';
 const Navbar = () => {
     const [nav,setNav] = useState(false)
     const handleClick = () => setNav(!nav);
@@ -38,9 +39,28 @@ const Navbar = () => {
             </div>
 
        {/* Social icons  */}
-       <div className='flex flex-column top-[35%] left-0'>
+       <div className= 'hidden lg:flex fixed flex-column top-[35%] left-0'>
             <ul>
-                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+           
+               <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                    <a target='_blank' className='flex justify-between items-center w-full text-gray-300'
+                     href="https://https://github.com/dim50dim?tab=overview&from=2025-08-01&to=2025-08-13">
+                        GitHub <FaGithub size={30}/> 
+                    </a>
+                 </li>
+                  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                    <a target='_blank' className='flex justify-between items-center w-full text-gray-300'
+                     href="mailto:zhelezkov.mit@proton.me">
+                        Email <HiOutlineMail size={30}/> 
+                    </a>
+                 </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                    <a target='_blank' className='flex justify-between items-center w-full text-gray-300'
+                     href="https://drive.google.com/file/d/12iJ8_zrBDRbf153hGTQCaU1jruoW8kfI/view?usp=drive_link">
+                        Resume <BsFillPersonLinesFill size={30}/> 
+                    </a>
+                 </li>
+             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
                     <a target='_blank' className='flex justify-between items-center w-full text-gray-300'
                      href="https://www.linkedin.com/in/dmitri-zelezkov-b35b41202">
                         Linkedin <FaLinkedin size={30}/> 
