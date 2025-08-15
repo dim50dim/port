8d9df697d70e6c9bee694182114c5b8bb0a01f84
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 13:14:42 +0200

    added conditional statement to mobile menu used !nav ? hidden : classes.

--- port/src/assets/components/Navbar.jsx
+++ port/src/assets/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from "@/assets/logo1.png"
 import {FaBars} from "react-icons/fa";
 const Navbar = () => {
     const [nav,setNav] = useState(false)
-    const handleClick = () => 
+    const handleClick = () => setNav(!nav);
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
           <div>
@@ -21,13 +21,13 @@ const Navbar = () => {
             
 
                {/* Hamburger  */}
-            <div className='md:hidden z-10'>
+            <div className='md:hidden z-10' onClick={handleClick}>
                 <FaBars/>
             </div>
 
             {/* Mobile menu  */}
             <div>
-                 <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center '>
+                 <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center "}>
               <li className='py-6 text-4xl'>Home</li>
               <li className='py-6 text-4xl'>About</li>
               <li className='py-6 text-4xl'>Skill</li>
