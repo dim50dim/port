0890013320e98ed4f5c60f1e2494cf86d86a6a20
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 10:38:19 +0200

    so, almost to all divs added hidden className.

--- port/src/assets/components/Navbar.jsx
+++ port/src/assets/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Logo from "@/assets/logo1.png"
+import {FaBars} from "react-icons/fa";
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
           <div>
               <img src={Logo} alt="Logo" style={{width:"50px"}} />
           </div>
-          <div> 
-            <ul>
+                {/* menu  */}
+          <div className='hidden'> 
+            <ul className='flex '>
               <li>Home</li>
               <li>About</li>
               <li>Skill</li>
@@ -17,7 +19,25 @@ const Navbar = () => {
       </div>         
 
                {/* Hamburger  */}
+            <div className='hidden'>
+                <FaBars/>
+            </div>
 
+            {/* Mobile menu  */}
+            <div>
+                 <ul className='hidden'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Skill</li>
+              <li>Work</li>
+              <li>Contact</li>
+          </ul>
+            </div>
+
+       {/* Social icons  */}
+       <div className='hidden'>
+
+       </div>
     </div>
   )
 }
