4c18e8b550968774afd3966ca81f7836c7216cda
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 19:41:55 +0200

    added logo to skills.

--- port/src/assets/components/Skill.jsx
+++ port/src/assets/components/Skill.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import HTML from "@/assets/html.png"
+import GIT from "@/assets/github.png"
+import REACT from "@/assets/react.png"
+import TAILWIND from "@/assets/tailwind.png"
+import NODE from "@/assets/node.png"
+import CSS from "@/assets/css.png"
+import JAVASCRIPT from "@/assets/javascript.png"
 const Skill = () => {
   return (
     <div>
@@ -16,7 +22,30 @@ const Skill = () => {
                             <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                             <p className='my-4'>HTML</p>
                        </div>
-                       
+
+                       <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                            <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+                            <p className='my-4'>CSS</p>
+                       </div>
+
+                       <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                            <img className='w-20 mx-auto' src={JAVASCRIPT} alt="JAVASCRIPT icon" />
+                            <p className='my-4'>JAVASCRIPT</p>
+                       </div>
+
+                       <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                            <img className='w-20 mx-auto' src={GIT} alt="GITHUB icon" />
+                            <p className='my-4'>GITHUB</p>
+                       </div>
+                       <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                            <img className='w-20 mx-auto' src={REACT} alt="REACT icon" />
+                            <p className='my-4'>REACT</p>
+                       </div>
+                       <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                            <img className='w-20 mx-auto' src={NODE} alt="NODE icon" />
+                            <p className='my-4'>NODE</p>
+                       </div>
+
                  </div>
              
            </div>
