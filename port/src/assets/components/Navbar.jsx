6b3860dc011bb613ecaeffe38d909deb9bc87d4b
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 10:09:10 +0200

    created list with info.

--- port/src/assets/components/Navbar.jsx
+++ port/src/assets/components/Navbar.jsx
@@ -4,8 +4,20 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
           <div>
-              <img src={Logo} alt="Logo" />
-          </div>         
+              <img src={Logo} alt="Logo" style={{width:"50px"}} />
+          </div>
+          <div> 
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Skill</li>
+              <li>Work</li>
+              <li>Contact</li>
+          </ul>
+      </div>         
+
+               {/* Hamburger  */}
+
     </div>
   )
 }
