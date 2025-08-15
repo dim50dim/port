5f064dd931e290672b0719e6bae00643eb0c58bd
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 13:11:18 +0200

    implemented useState and handleClick function.

--- port/src/assets/components/Navbar.jsx
+++ port/src/assets/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "@/assets/logo1.png"
 import {FaBars} from "react-icons/fa";
 const Navbar = () => {
+    const [nav,setNav] = useState(false)
+    const handleClick = () => 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
           <div>
@@ -19,18 +21,18 @@ const Navbar = () => {
             
 
                {/* Hamburger  */}
-            <div className='md:hidden'>
+            <div className='md:hidden z-10'>
                 <FaBars/>
             </div>
 
             {/* Mobile menu  */}
             <div>
-                 <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center '>
-              <li>Home</li>
-              <li>About</li>
-              <li>Skill</li>
-              <li>Work</li>
-              <li>Contact</li>
+                 <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center '>
+              <li className='py-6 text-4xl'>Home</li>
+              <li className='py-6 text-4xl'>About</li>
+              <li className='py-6 text-4xl'>Skill</li>
+              <li className='py-6 text-4xl'>Work</li>
+              <li className='py-6 text-4xl'>Contact</li>
           </ul>
             </div>
 
