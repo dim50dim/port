960a5f9d0e51579ac61293d2ffeddc8e974ad74e
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 14:35:34 +0200

    in Home container added p and h1 tags.

--- port/src/assets/components/Home.jsx
+++ port/src/assets/components/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi';
 const Home = () => {
   return (
-    <div className='bg-[#0a192f] w-full h-screen '>
-        
+    <div name='home' className='bg-[#0a192f] w-full h-screen '>
+               {/* Container  */}
+
+               <div className='ma-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'></div>
     </div>
   )
 }
