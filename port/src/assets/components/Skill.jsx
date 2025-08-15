2c414c1c0c319d1b117daffb33c1b4bbc3a6df7f
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 19:45:23 +0200

    made a mistakes in container of logo-skills.

--- port/src/assets/components/Skill.jsx
+++ port/src/assets/components/Skill.jsx
@@ -16,7 +16,7 @@ const Skill = () => {
                   <p className='text-4xl  py-4 font-bold inline border-b-4 border-pink-600'>Experience</p>
                   <p className='py-4'> These are things I've used to build peculiar stuff</p>
                </div>
-                 <div className='w-full grid grid-col-2 sm:grid-cols4 gap-4 text-center py-8'>
+                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
 
                        <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                             <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
