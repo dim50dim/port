525a79823837d83e1a75e5a8ea9327c09e184561
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-15 18:05:45 +0200

    upgraded content to multiply it.

--- port/src/assets/components/Skill.jsx
+++ port/src/assets/components/Skill.jsx
@@ -3,16 +3,16 @@ import HTML from "@/assets/html.png"
 const Skill = () => {
   return (
     <div>
-   <div name="skills">
+   <div name="skills" className='bg-[#0a192f]'>
            {/* Container ? */}
-           <div>
-               <div>
-                  <p>Experience</p>
+           <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+               <div className='text-white'>
+                  <p className='text-4xl text-gray-300 py-4'>Experience</p>
                   <p> These are things I've used to build peculiar stuff</p>
                </div>
                  <div>
                        <div>
-                            <img className='w-[50px]' src={HTML} alt="HTML icon" />
+                            <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                        </div>
                  </div>
            </div>
