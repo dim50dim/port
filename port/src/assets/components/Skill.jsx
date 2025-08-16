6f33f948b926d51c500fde97ae2826bc0b4c9969
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-16 21:34:57 +0200

    rewrote Skill component , no padding bottom

--- port/src/assets/components/Skill.jsx
+++ port/src/assets/components/Skill.jsx
@@ -13,7 +13,7 @@ const Skill = () => {
            {/* Container ? */}
            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                <div >
-                  <p className='text-4xl  py-4 font-bold inline border-b-4 border-pink-600'>Experience</p>
+                  <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
                   <p className='py-4'> These are things I've used to build peculiar stuff</p>
                </div>
                  <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
