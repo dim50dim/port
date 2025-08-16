edaf9bf580b553187412d8b18e7e0cc72af31976
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-16 20:57:32 +0200

    added Link from react-scroll.

--- port/src/assets/components/Navbar.jsx
+++ port/src/assets/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Logo from "@/assets/logo1.png"
 import {FaBars, FaTimes,FaGithub,FaLinkedin} from "react-icons/fa";
 import {BsFillPersonLinesFill} from "react-icons/bs"
 import { HiOutlineMail } from 'react-icons/hi';
+import {Link} from "react-scroll";
 const Navbar = () => {
     const [nav,setNav] = useState(false)
     const handleClick = () => setNav(!nav);
@@ -14,11 +15,21 @@ const Navbar = () => {
                 {/* menu  */}
          
             <ul className='hidden md:flex '>
-              <li>Home</li>
-              <li>About</li>
-              <li>Skill</li>
-              <li>Work</li>
-              <li>Contact</li>
+              <li>
+                <Link to="home" smooth={true} duration={500}>Home</Link>
+              </li>
+              <li>
+                <Link to="about" smooth={true} duration={500}>About</Link>
+              </li>
+              <li>
+                <Link to="skill" smooth={true} duration={500}>Skills</Link>
+              </li>
+              <li>
+                <Link to="work" smooth={true} duration={500}>Work</Link>
+              </li>
+              <li>
+                <Link to="contact" smooth={true} duration={500}>Contact</Link>
+              </li>
           </ul>
             
 
