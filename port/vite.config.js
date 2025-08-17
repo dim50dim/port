80d3dc3a5ccce7c902aa3614a73fb074f8bf4861
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-17 08:45:16 +0200

    fixed syntax error in Home component.

--- port/vite.config.js
+++ port/vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/port/",
+
   plugins: [react(),tailwindcss()],
   resolve: {
     alias:{
       "@":'/src'
     }
   }
-})
+   })
