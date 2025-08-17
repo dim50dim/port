fa36f2c8ca2c0230c898ba32449587a693ec732b
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-17 15:00:19 +0200

    changed the place of block with photo.

--- port/src/assets/components/Home.jsx
+++ port/src/assets/components/Home.jsx
@@ -8,12 +8,6 @@ const Home = () => {
          
            {/* photo  */}
 
-             <div class="flex flex-col items-center mt-10">
-
-  <div class="w-40 h-40 rounded-full  bg-cover shadow-lg mb-6">
-    <div style={{backgroundImage:`url(${myPhoto})`}} alt="My Photo" className="w-40 h-40 rounded-full bg-center bg-cover shadow-lg border-4 border-white"></div>
-  </div>
-    </div>
 
                      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-pink-600 '>Hi, my name is</p>
@@ -24,6 +18,13 @@ const Home = () => {
                       designing) exceptional digital experiences. Currently, I’m focused on
                       building responsive full-stack web applications.
                        </p>
+                                    <div class="flex flex-col items-center mt-10">
+
+  <div class="w-40 h-40 rounded-full  bg-cover shadow-lg mb-6">
+    <div style={{backgroundImage:`url(${myPhoto})`}} alt="My Photo" className="w-40 h-40 rounded-full bg-center bg-cover shadow-lg border-4 border-white"></div>
+  </div>
+    </div>
+
                        <div>
                            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
                             View Work  <span className=' group-hover:rotate-90  duration-300'> <HiArrowNarrowRight className='ml-3'/></span></button>
