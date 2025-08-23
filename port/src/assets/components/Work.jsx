3b4bf03567b48db0664d2144b4385504fca384c5
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-23 12:53:17 +0200

    implemented map().

--- port/src/assets/components/Work.jsx
+++ port/src/assets/components/Work.jsx
@@ -6,10 +6,29 @@ const Work = () => {
 
     return (
         <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                 <div className="pb-8">
+                     <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">Work</p>
+                             <p className="py-6">Look out some of my recent projects</p>
+                
+                  {/* all projects  */}
+                  <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+                   {project.map((item,index) => (
+                      <div 
+                       key={index}
+                       style={{backgroundImage:`url(${item.image})`}}
+                      className="shadow-lg shadow-[#040c16] group container rounded-md
+                       flex justify-center text-center items-center mx-auto content-div"></div>
+                   ) )}
 
+                  </div>
+
+                 </div>
+             </div>
         </div>
     )
 }
+export default Work;
 // const Work = () => {
 //   return (
 //     <div name="work" className='bg-[#0a192f] md:h-screen w-full text-gray-300'>
